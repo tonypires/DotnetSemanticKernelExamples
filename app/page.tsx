3b995e64e82d78eaa5c587e0db42a263153d0ca3
Tony Pires <ampires9@gmail.com>
2023-10-04 22:27:12 +0100

    WIP:  Designed a basic UI to host various ai pocs.

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,98 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import Image from 'next/image';
+import styles from './page.module.css';
+import {
+    Box,
+    Container,
+    Stack,
+    Tab,
+    Tabs,
+    TextField,
+    Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import AISQLAgent from './components/AISQLAgent';
+import Link from 'next/link';
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [value, setValue] = useState(0);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+    return (
+        <>
+            <Container className={styles.main}>
+                <Stack spacing={2}>
+                    <Typography variant="h3" textAlign={'center'}>
+                        AI POCs
+                    </Typography>
+                    <Box sx={{ width: '100%' }}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <Tabs value={value} onChange={handleChange}>
+                                <Tab label="AI SQL" {...a11yProps(0)} />
+                                <Tab label="AI PDF" {...a11yProps(1)} />
+                                <Tab
+                                    label="AI App Documentation"
+                                    {...a11yProps(2)}
+                                />
+                                <Tab label="AI PowerPoint" {...a11yProps(3)} />
+                            </Tabs>
+                        </Box>
+                        <CustomTabPanel value={value} index={0}>
+                            <AISQLAgent />
+                        </CustomTabPanel>
+                        <CustomTabPanel value={value} index={1}>
+                            Upload a pdf file and ask questions against it
+                        </CustomTabPanel>
+                        <CustomTabPanel value={value} index={2}>
+                            Specify the path to an application (e.g. a node app
+                            or c# web api) for it to read the contents and put
+                            together some documentation.
+                        </CustomTabPanel>
+                        <CustomTabPanel value={value} index={3}>
+                            Describe a powerpoint presentation to generate.
+                            Build a form which describes how many slides (at
+                            least), how concise it should be, etc. Look at{' '}
+                            <Link
+                                href="https://github.com/CyberTimon/Powerpointer-For-Local-LLMs/tree/main"
+                                target="_blank"
+                            >
+                                https://github.com/CyberTimon/Powerpointer-For-Local-LLMs/tree/main
+                            </Link>
+                        </CustomTabPanel>
+                    </Box>
+                </Stack>
+            </Container>
+        </>
+    );
 }
